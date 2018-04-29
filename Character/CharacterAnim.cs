cd449c40d8d60c1b82ced32fb7ac9449b3023a60
dstovell <dstovell@gmail.com>
2018-04-28 21:04:02 -0700

    Initial commit

---
+++ Character/CharacterAnim.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnim : MonoBehaviour 
+{
+	public Animator Anim;
+	public bool Run = false;
+	public bool Walk = false;
+	public bool Idle = false;
+
+	private List<string> Animations = new List<string> ();
+
+	public void AddAnimation(string name)
+	{
+		this.Animations.Add(name);
+	}
+
+	public void SetAnimation(string name)
+	{
+		for (int i=0; i<this.Animations.Count; i++) 
+		{
+			if (this.Animations[i] == name)
+			{
+				this.Anim.SetBool(this.Animations[i], true);
+			}
+			else
+			{
+				this.Anim.SetBool(this.Animations[i], false);
+			}
+		}
+	}
+
+	void Start() 
+	{
+		this.SetupAnimations();
+	}
+
+	void SetupAnimations()
+	{
+		AddAnimation("Idle");
+		AddAnimation("Walk");
+		AddAnimation("Run");
+	}
+
+	void Update() 
+	{
+		if (this.Run) 
+		{
+			this.SetAnimation("Run");
+			this.Run = false;
+		}
+		else if (this.Walk) 
+		{
+			this.SetAnimation("Walk");
+			this.Walk = false;
+		}
+		else if (this.Idle) 
+		{
+			this.SetAnimation("Idle");
+			this.Idle = false;
+		}
+	}
+}
---
+++ TestCharacter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCharacter : MonoBehaviour 
+{
+	public Animator Anim;
+	public bool Run = false;
+	public bool Walk = false;
+	public bool Idle = false;
+
+	void Start () 
+	{
+	}
+		
+	void DisableAnimations ()
+	{
+		Anim.SetBool ("Idle", false);
+		Anim.SetBool ("Walk", false);
+		Anim.SetBool ("Run", false);
+	}
+
+	void Update () 
+	{
+		if (this.Run) 
+		{
+			DisableAnimations();
+			Anim.SetBool("Run", true);
+			this.Run = false;
+		}
+		else if (this.Walk) 
+		{
+			DisableAnimations();
+			Anim.SetBool("Walk", true);
+			this.Walk = false;
+		}
+		else if (this.Idle) 
+		{
+			DisableAnimations();
+			Anim.SetBool("Idle", true);
+			this.Idle = false;
+		}
+	}
+}
